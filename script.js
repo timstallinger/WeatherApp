964d3f5f3adc6658b440f22afefd9f0df2f687af
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-11-05 17:04:53 +0100

    translation through API

--- script.js
+++ script.js
@@ -9,57 +9,18 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
             + city
-            + "&units=metric&appid="
+            + "&lang=de&units=metric&appid="
             + this.apiKey
         )
         .then((response) => response.json())
         .then((data) => this.displayWeather(data));
     },
-    translateDescription: function (description) {
-        // translate the weather description into German
-        switch (description) {
-            case "clear sky":
-                description = "klarer Himmel";
-                break;
-            case "few clouds":
-                description = "leicht bewölkt";
-                break;
-            case "scattered clouds":
-                description = "wolkig";
-                break;
-            case "broken clouds":
-                description = "stark bewölkt";
-                break;
-            case "overcast clouds":
-                description = "leicht bewölkt";
-                break;c
-            case "shower rain":
-                description = "Regenschauer";
-                break;
-            case "rain":
-                description = "Regen";
-                break;
-            case "thunderstorm":
-                description = "Gewitter";
-                break;
-            case "snow":
-                description = "Schnee";
-                break;
-            case "mist":
-                description = "Nebel";
-                break; 
-            // default is the original description
-        }
-        return description;
-    },
     displayWeather: function (data) {
         // display the weather data on the screen
         const { name } = data;
-        var { icon, description } = data.weather[0];
+        const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-
-        description = this.translateDescription(description);
         
         // set text contents
         document.querySelector(".location-place").innerText = "Wetter in " + name;
