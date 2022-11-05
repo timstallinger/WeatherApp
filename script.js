12d0d958839b2d2e30fb4444c30bcdc7410b8a79
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-11-05 16:35:29 +0100

    Wetter empfangen, labels setzen und background

--- script.js
+++ script.js
@@ -15,8 +15,63 @@ let weather = {
             + "&units=metric&appid="
             + this.apiKey
         )
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        console.log(data);
+        var { name } = data;
+        var { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        // translate description into German with switch
+        switch (description) {
+            case "clear sky":
+                description = "klarer Himmel";
+                break;
+            case "few clouds":
+                description = "leicht bewölkt";
+                break;
+            case "scattered clouds":
+                description = "wolkig";
+                break;
+            case "broken clouds":
+                description = "stark bewölkt";
+                break;
+            case "overcast clouds":
+                description = "leicht bewölkt";
+                break;c
+            case "shower rain":
+                description = "Regenschauer";
+                break;
+            case "rain":
+                description = "Regen";
+                break;
+            case "thunderstorm":
+                description = "Gewitter";
+                break;
+            case "snow":
+                description = "Schnee";
+                break;
+            case "mist":
+                description = "Nebel";
+                break; 
+        }
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".location-place").innerText = "Wetter in " + name;
+        document.querySelector(".location-date").innerText = new Date().toDateString();
+        document.querySelector(".current-weather").innerText = "Beschreibung: " + description;
+        document.querySelector(".current-temp").innerText = temp + "°C";
+        document.querySelector(".current-humidity").innerText =
+            "Feuchtigkeit: " + humidity + "%";
+        document.querySelector(".current-wind").innerText =
+            "Wind: " + speed + " km/h";
+        // make description api readable
+        description = data.weather[0]["description"].replace(" ", ",");
+        name = name.replace(" ", ",");
+        // set background image according to weather and city from unsplash.com
+        document.body.style.backgroundImage =
+            "url('https://source.unsplash.com/1600x900/?" + description +  "')";
     },
 };
-weather.fetchWeather("Frankfurt");
+weather.fetchWeather("Mainz");
